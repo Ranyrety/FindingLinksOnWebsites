0176f3e8f29fa900f0614b5862cba8c148292e98
Michał <mihau.ruminski@gmail.com>
2018-06-26 21:49:37 +0200

    verryyy basic scraping

--- FindStringInHTML/Program.cs
+++ FindStringInHTML/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace FindStringInHTML
 {
@@ -10,6 +13,54 @@ namespace FindStringInHTML
 	{
 		static void Main(string[] args)
 		{
+			TestClass test = new TestClass("https://www.resetera.com/forums/video-games.7/", "href");
+
+			test.WriteToConsole();
+
+			Console.ReadLine();
+			
 		}
 	}
+
+
+	public class TestClass
+	{
+		string adress;
+		Regex regex; 
+
+		public TestClass(string adr, string reg)
+		{
+			adress = adr;
+			regex = new Regex(reg);
+		}
+
+		public async void WriteToConsole()
+		{
+			HttpClient client = new HttpClient();
+			List<string> lines = new List<string>();
+			using (var stream = await client.GetStreamAsync(adress))
+			{
+				using(var reader = new StreamReader(stream))
+				{
+					while(!reader.EndOfStream)
+					{
+						string line = await reader.ReadLineAsync();
+						if (regex.IsMatch(line))
+						{
+							lines.Add(line);
+							//Console.WriteLine(line);
+						}
+					}
+				}
+			}
+			foreach(string l in lines)
+			{
+				Console.WriteLine(l);
+				Console.WriteLine();
+			}
+
+		}
+	}
+
+
 }
