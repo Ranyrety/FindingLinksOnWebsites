42b2b6b649270d8e1da73739a247282fc7b09a62
Michał <mihau.ruminski@gmail.com>
2018-06-28 18:44:09 +0200

    Explorating problem of getting links from web sites

--- FindStringInHTML/Program.cs
+++ FindStringInHTML/Program.cs
@@ -6,17 +6,27 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace FindStringInHTML
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static  void Main(string[] args)
 		{
-			TestClass test = new TestClass("https://www.resetera.com/forums/video-games.7/", "^<a.+>\\b");
+			TestClass test = new TestClass("https://www.resetera.com/threads/steamspy-is-now-sharing-specific-numbers-again-experimental.52068/", "^<a.+/>");
 
-			test.WriteToConsole();
-
+			//test.WriteToConsole();
+			List<string> mediaList = new List<string>();
+			Task task = Task.Run(() => test.PrintMediaPaths());
+			while(!task.IsCompleted)
+			{
+				Console.WriteLine("waiting data to be processed");
+			}
+			
+			//iterate over collection and check if it contains multimedia files links
+			
+			
 			Console.ReadLine();
 			
 		}
@@ -28,16 +38,22 @@ namespace FindStringInHTML
 		string adress;
 		Regex regex;
 		List<string> lines = new List<string>();
-		List<string> matches = new List<string>();
+		//List<string> matches = new List<string>();
+		public List<string> MediaPath { get; private set; }
+
+		public List<string> Matches { get; private set; }
 
 		public TestClass(string adr, string reg)
 		{
 			adress = adr;
 			regex = new Regex(reg);
+			MediaPath = new List<string>();
+			Matches = new List<string>();
 		}
 
-		public async void WriteToConsole()
+		private async Task WriteToConsole()
 		{
+			List<string> matches = new List<string>();
 			HttpClient client = new HttpClient();
 			
 			using (var stream = await client.GetStreamAsync(adress))
@@ -49,21 +65,48 @@ namespace FindStringInHTML
 						string line = await reader.ReadLineAsync();
 						if (regex.IsMatch(line))
 						{
-							Match match = Regex.Match(line, regex.ToString());
-							lines.Add(match.Value);
-							Match m = Regex.Match(match.Value, "(?!=href=)\\w+\\W{1}\\w+");
-							Console.WriteLine(m.Value);
-							matches.Add(m.Value);
+							if (Regex.Match(line, "<img.+>").Success)
+							{
+								Match match = Regex.Match(line, "<img.+>");
+								lines.Add(match.Value);
+								Match m = Regex.Match(match.Value, "(?<=src=\")\\w+\\W{1}\\.+\"");
+								Console.WriteLine(m.Value);
+								Matches.Add(m.Value);
+							}
 						}
 					}
 				}
 			}
-			foreach(string l in matches)
-			{
-				Console.WriteLine(l);
-				Console.WriteLine();
-			}
+			//foreach(string l in matches)
+			//{
+			//	Console.WriteLine(l);
+			//	Console.WriteLine();
+			//}
 
+		}
+
+		private async Task GetMediaPath()
+		{
+			//Task task = Task.Run(() => WriteToConsole());
+			
+
+			foreach(var i in Matches)
+			{
+				if(i.Contains(".jpg") | i.Contains(".gif"))
+				{
+					MediaPath.Add(i);
+				}
+			}
+		}
+
+		public async Task PrintMediaPaths()
+		{
+			await WriteToConsole();
+			await GetMediaPath();
+			foreach(var i in MediaPath)
+			{
+				Console.WriteLine(i);
+			}
 		}
 	}
 
